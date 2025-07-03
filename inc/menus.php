2d239443542402627d2742a3e760a7c2bf8dfd9f
Nathan Smith <nathanielvs@gmail.com>
2025-07-03 10:39:33 -0600

    feature: Add dynamic classes to menu items and links based on menu location.

--- inc/menus.php
+++ inc/menus.php
@@ -3,6 +3,8 @@
  * Theme Navigation Menus
  *
  * Registers navigation menu locations for use with wp_nav_menu().
+ * 
+ * Adds dynamic classes to menu items & links based on menu location.
  *
  * @package Groundwork
  */
@@ -12,16 +14,35 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+// Register navigation menus
 function axon_register_menus() {
-     // Register navigation menus
     register_nav_menus( array(
         'header' => __( 'Header Menu', 'axon' ),
         'footer'  => __( 'Footer Menu', 'axon' ),
-        'primary' => __( 'Primary Navigation Menu', 'axon' ),
-        'secondary' => __( 'Secondary Navigation Menu', 'axon' ),
         'social' => __( 'Social Media Menu', 'axon' ),
-        'utility' => __( 'Utility Menu', 'axon' )
+        'top-bar' => __( 'Top Bar Menu', 'axon' )
     ) );
 }
-
 add_action( 'after_setup_theme', 'axon_register_menus' );
+
+// Add menu item classes based on menu location
+function axon_menu_item_classes( $classes, $item, $args, $depth ) {
+    if ( isset( $args->theme_location ) ) {
+        $classes[] = $args->theme_location . '-menu__item';
+    }
+
+    return $classes;
+}
+
+// 10 = standard priority, 4 = number of args from above function that `add_filter` needs to pass
+add_filter( 'nav_menu_css_class', 'axon_menu_item_classes', 10, 4 );
+
+// Add link classes for menu items based on menu location
+function axon_menu_link_attributes( $atts, $item, $args, $depth ) {
+    if ( isset( $args->theme_location ) ) {
+        $atts['class'] = $args->theme_location . '-menu__link';
+    }
+    
+    return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'axon_menu_link_attributes', 10, 4 );
