91c0b97b5b45727bc56a657604afc4161b4c4a1e
Nathan Smith <nathanielvs@gmail.com>
2025-07-02 23:35:28 -0600

    feature: enqueue scripts and add functionality to use additional stylesheets

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -12,8 +12,44 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+// Enqueue stylesheets
 function axon_enqueue_styles() {
-    wp_enqueue_style( 'axon-style', get_stylesheet_uri() );
-};
+    // Get theme version
+    $theme = wp_get_theme();
+    $theme_version = $theme->get( 'Version' );
 
+    // Enqueue root stylesheet first
+    wp_enqueue_style( 
+        'axon-style', 
+        get_stylesheet_uri(),
+        [], // No dependencies
+        $theme_version
+    );
+
+    /*
+    // Enqueue additional stylesheet in assets/css
+    wp_enqueue_style(
+        // Create a theme-prefixed handle with the name of the stylesheet and update the directory URI below
+        'axon-addStylesheetNameHere',
+        get_template_directory_uri() . '/assets/css/addStylesheetNameHere.css',
+        // List stylesheets in order by which they should be loaded, starting with the root stylesheet
+        [ 'axon-style' ],
+        // Version of this CSS file
+        '1.0.0'
+    );
+    */
+}
 add_action( 'wp_enqueue_scripts', 'axon_enqueue_styles' );
+
+// Enqueue scripts
+function axon_enqueue_scripts() {
+    // Enqueue the `main.js` file
+    wp_enqueue_script(
+        'axon-main',
+        get_template_directory_uri() . '/assets/js/main.js',
+        ['jquery'],
+        null, // Don't add a version
+        true // Load in footer
+    );
+}
+add_action( 'wp_enqueue_scripts', 'axon_enqueue_scripts' );
