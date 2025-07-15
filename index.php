9ca53f5b1b365d0efaf74178adfb3f24bfe33842
Nathan Smith <nathanielvs@gmail.com>
2025-07-14 22:18:01 -0600

    chore: move post-loop logic into the content-post template, add additional attr for improved accessibility

--- index.php
+++ index.php
@@ -19,42 +19,9 @@ get_header();
 <main class="site-main" role="main" aria-label="<?php echo esc_attr_x( 'Main content', 'ARIA label for main region', theme_get_text_domain() ); ?>">
     <?php get_template_part( 'templates/parts/hero', 'global' ); ?>
 
-    <div class="container site-main__container">
-        <div class="row site-main__row">
-            <!-- If posts exist -->
-            <?php if ( have_posts() ) : ?>
-                <div class="col site-main__col site-main__col--post">
-                    <?php while ( have_posts() ) :
-                        the_post();
-                    ?>
-                        <article class="post-item">
-                            <?php get_template_part( 'templates/parts/content', get_post_type() ); ?>
-                        </article>
-                    <?php endwhile; ?>
-                </div> <!-- end site-main__col--post -->
-
-                <div class="col site-main__col site-main__nav-col">
-                    <nav class="site-pagination" role="navigation" aria-label="<?php echo esc_attr_e( 'Posts navigation', 'axon' ); ?>">
-                        <?php
-                            the_posts_pagination( array(
-                                'mid_size'           => 2,
-                                'prev_text'          => __( '« Prev', 'axon' ),
-                                'next_text'          => __( 'Next »', 'axon' ),
-                                'screen_reader_text' => __( 'Posts navigation', 'axon' ),
-                            ) );
-                        ?>
-                    </nav>
-                </div><!-- end site-main__nav-col -->
-                
-            <!-- If no posts exist -->
-            <?php else : ?>
-                <div class="col site-main__col site-main__col--empty">
-                    <?php get_template_part( 'templates/parts/content', 'none' ); ?>
-                </div> <!-- end site-main__col--empty -->
-            <?php endif; ?>
-        </div> <!-- end site-main__row -->
-    </div> <!-- end site-main__container -->
+    <?php if ( have_posts() ) : ?>
+            <?php get_template_part( 'templates/parts/content', 'post' ); ?>
+    <?php endif; ?>
 </main>
 
-<?php
-get_footer();
+<?php get_footer(); ?>
--- templates/parts/content-post.php
+++ templates/parts/content-post.php
@@ -8,31 +8,44 @@
  *
  * @package Axon
  */
+
+if ( get_post_type() !== 'post' ) {
+    return;
+}
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'content-post' ); ?>>
-    <div class="content-post__title-wrap">
-        <h2 class="content-post__title">
-            <a href="<?php the_permalink(); ?>" rel="bookmark">
-                <?php the_title(); ?>
-            </a>
-        </h2>
-
-        <?php if ( get_post_type() === 'post' ) : ?>
-            <div class="content-post__meta">
-                <span class="content-post__date"><?php echo get_the_date(); ?></span>
-                <span class="content-post__author"><?php the_author(); ?></span>
-            </div>
-        <?php endif; ?>
-    </div>
-
-    <div class="content-post__body">
-        <?php the_excerpt(); ?>
-    </div>
-
-    <div class="content-post__readmore-wrap">
-        <a class="content-post__readmore" href="<?php the_permalink(); ?>">
-            <?php esc_html_e( 'Read More', 'axon' ); ?>
-        </a>
-    </div>
-</article>
+<section class="post-list" role="region" aria-label="<?php echo esc_attr_x( 'Post content', 'ARIA label for dynamic posts section', theme_get_text_domain() ); ?>">
+    <div class="container post-list__container">
+        <div class="row post-list__row">
+            <div class="col post-list__col">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class( 'post-preview' ); ?>>
+                        <div class="post-preview__title-wrap">
+                            <h3 id="post-title-<?php the_ID(); ?>" class="post-preview__title">
+                                <a href="<?php the_permalink(); ?>" rel="bookmark">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h3>
+                            
+                            <div class="post-preview__meta">
+                                <span class="post-preview__date"><?php echo esc_html( get_the_date() ); ?></span>
+                        
+                                <span class="post-preview__author"><?php echo esc_html( get_the_author() ); ?></span>
+                            </div>
+                        </div>
+                
+                        <div class="post-preview__body">
+                            <?php the_excerpt(); ?>
+                        </div>
+                
+                        <div class="post-preview__readmore-wrap">
+                            <a class="post-preview__readmore" href="<?php the_permalink(); ?>" rel="bookmark" aria-describedby="post-title-<?php the_ID(); ?>">
+                                <?php esc_html_e( 'Read More', theme_get_text_domain() ); ?>
+                            </a>
+                        </div>
+                    </article>
+                <?php endwhile; ?>
+            </div> <!-- end post-list__col -->
+        </div> <!-- end post-list__row -->
+    </div> <!-- end post-list__container -->
+</section>
